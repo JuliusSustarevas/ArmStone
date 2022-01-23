8d49d6f6b262fc90a080cd00444c0a53ac9eeb82
Julius <julius1618@gmail.com>
2022-01-23 12:23:39 +0000

    wip localisation new

--- as_control/as_extruder_control/arduino/extruder/extruder.ino
+++ as_control/as_extruder_control/arduino/extruder/extruder.ino
@@ -16,32 +16,27 @@ volatile int pulse_per_second;
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 ros::NodeHandle nh;
 
-void volumeMessageCb( const std_msgs::Float32& msg){
-  pulse_per_second=min(max(int(msg.data*200*3),min_pulse),max_pulse);// TODO figure out what this should be.
-}
-
 void pulseMessageCb( const std_msgs::Int32& msg){
   if (msg.data>0){
-    pulse_per_second=min(max(abs(msg.data),min_pulse),max_pulse);
+    pulse_per_second=-min(max(abs(msg.data),min_pulse),max_pulse);
   }else if (msg.data<0)
   {
-    pulse_per_second=-min(max(abs(msg.data),min_pulse),max_pulse);
+    pulse_per_second=min(max(abs(msg.data),min_pulse),max_pulse);
   }else{
     pulse_per_second=0;
   } 
   
 }
 
-ros::Subscriber<std_msgs::Float32> sub_volume_rate("set_volume_rate", &volumeMessageCb );
-ros::Subscriber<std_msgs::Int32> sub_pulse_rate("set_pulse_rate", &pulseMessageCb );
+ros::Subscriber<std_msgs::Int32> sub_pulse_rate("as_extruder/set_pulse_rate", &pulseMessageCb );
 
 void setup() {
   stepper.setMinPulseWidth(20);
   stepper.setMaxSpeed(1500);
   stepper.setAcceleration(200);
-  nh.initNode();
-  nh.subscribe(sub_volume_rate);
+  nh.initNode();  
   nh.subscribe(sub_pulse_rate);
+  pulse_per_second=1000;
 }
 void loop() {  
   stepper.setSpeed(pulse_per_second);
